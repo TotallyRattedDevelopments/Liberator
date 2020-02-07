093dce486822598ec20356383fbadb3d84957480
Kev McCarthy <developments@totallyratted.com>
2020-02-07 11:42:42 +0000

    Add enum

---
+++ dotnet/Liberator.Driver/Enums/EnumElementRelationships.cs
@@ -0,0 +1,45 @@
+﻿// EnumElementRelationships.cs
+//
+// Author:
+//       kevmccarthy <developments@totallyratted.com>
+//
+// Copyright (c) 2020 Liberator Test Tools
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+
+namespace Liberator.Driver.Enums
+{
+    public enum EnumElementRelationships
+    {
+        NoResponse = 0,
+
+        NoRelationship = 1,
+
+        FirstAfterSecond = 2,
+
+        FirstBeforeSecond = 4,
+
+        FirstInsideSecond = 8,
+
+        FirstOutsideSecond = 16,
+
+        Unknown = 32
+    }
+}
